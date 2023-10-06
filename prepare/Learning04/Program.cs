6f5df8360025359552a27056aee531f7281074ea
I-RICHWEB <ize20002@byui.edu>
2023-10-06 01:20:50 +0100

    learning activity for week-04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.WriteLine();
+        Assignment assignment1 = new("Uyiosa Richmond", "Programming with classes");
+        Console.WriteLine(assignment1.GetSummary());
+
+
+        Console.WriteLine();
+        MathAssignment math = new("Uyiosa Richmond", "Subtraction", "3.7", "3-5");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeWorkList());
+
+        Console.WriteLine();
+        WritingAssignment writing = new("Izekor Stone","Social Science", "The Causes Of Depression In Youth");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
+        Console.WriteLine();
     }
 }
